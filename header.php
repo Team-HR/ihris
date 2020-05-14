7d089bad8452d3d8265c92f5f16d6ac6ca4886f4
Franz Joshua A Valencia <franzval123@gmail.com>
2020-05-14 10:12:55 +0800

    wip: Change vue to prod mode.

--- header.php
+++ header.php
@@ -17,9 +17,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true && $_SESSION[
   <meta name="viewport" content="width=device-width, height=device-height , initial-scale=1">
   <link rel="stylesheet" type="text/css" href="ui/dist/semantic.css">
 
-  <!-- <script src="node_modules\vue\dist\vue.min.js"></script> -->
+  <script src="node_modules\vue\dist\vue.min.js"></script>
   <!-- development version, includes helpful console warnings -->
-  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
   <script src="node_modules\moment\min\moment.min.js"></script>
 
   <script src="jquery/jquery-3.3.1.min.js"></script>
