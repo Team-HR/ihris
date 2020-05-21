4e7d8da01ab8322c63a098df5bd93acc2352851f
pascual <japorms53@gmail.com>
2020-05-21 14:50:28 +0800

    feedback phase 1

--- umbra/feedback/jason.php
+++ umbra/feedback/jason.php
@@ -1,18 +1,18 @@
 <?php
     require_once "../../_connect.db.php";
-    if(isset($_POST['getEmpList'])){
-        $period = $_POST['per'];
-        $year = $_POST['yr'];
-        // $sql = 
+    if(isset($_POST['getFeedback'])){
+        $sql = "SELECT * from `spms_feedbacking` where `feedbacking_year`='$_POST[year]'";
+        echo a($sql);        
+    }else{
+        $sql = "SELECT * from `employees` ORDER BY `employees`.`employmentStatus` ASC , lastName ASC";
+        echo a($sql);
     }
-    elseif(isset($_POST['empSearch'])){
-        $search = $_POST['empSearch'];
-        $sql = "SELECT * FROM `employees` where `firstName` like '$search%' or `lastName` like '$search%'";
-        $sql = $mysqli->query($sql);
-        $ar = [];
-        while($row = $sql->fetch_assoc()){
-            $ar[] = $row;
+    function a($sql){
+        $sql = $GLOBALS['mysqli']->query($sql);
+        $a = [];
+        while($ar = $sql->fetch_assoc()){
+            $a[] = $ar;
         }
-        echo json_encode($ar);
+        return json_encode($a);
     }
 ?>
