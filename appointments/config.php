dbbeab3819f11d441e7f583028f172a0b34b2f66
Franz Joshua A Valencia <franzval123@gmail.com>
2020-05-21 16:30:04 +0800

    wip

--- appointments/config.php
+++ appointments/config.php
@@ -1,28 +1,4 @@
-<!-- id
-plantilla_id
-employee_id
-status_of_appointment
-date_of_appointment
-date_ended
-nature_of_appointment
-legal_doc
-memo_for_legal
-head_of_agency
-date_of_signing
-csc_auth_official
-date_signed_by_csc
-csc_mc_no
-published_at
-date_of_publication
-hrmo
-screening_body
-date_of_screening
-committee_chair
-notation_1
-notation_2
-notation_3
-notation_4
-csc_release_date -->
+
 <?php
 require_once '../_connect.db.php';
 
@@ -37,11 +13,24 @@ elseif(isset($_POST['getPlantillas'])){
 }
 
 
-function createAppointment($json,$mysqli){
-    // check if employee is existing or not
-    // if existing update appointment
-    // if not create new employee get id and insert to appointments
+function newEmployeeNewAppointment($json,$mysqli){
+    // insert new empoyee to employees table
+    $sql = "INSERT INTO `employees` (`employees_id`, `firstName`, `lastName`, `middleName`, `extName`) VALUES (NULL, ?, ?, ?, ?)";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('ssss',$json['first_name'],$json['last_name'],$json['middle_name'],$json['ext_name']);
+    $stmt->execute();
+    $stmt->();
+    // get insert_id
+    $sql = "INSERT INTO `appointments` (`id`, `plantilla_id`, `employee_id`, `status_of_appointment`, `date_of_appointment`, `date_ended`, `nature_of_appointment`, `legal_doc`, `memo_for_legal`, `head_of_agency`, `date_of_signing`, `csc_auth_official`, `date_signed_by_csc`, `csc_mc_no`, `published_at`, `date_of_publication`, `hrmo`, `screening_body`, `date_of_screening`, `committee_chair`, `notation_1`, `notation_2`, `notation_3`, `notation_4`, `csc_release_date`, `timestamp_created`, `timestamp_updated`) VALUES (NULL, ?, ?, ?, ?, NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, current_timestamp(), current_timestamp())";
+
 }
+function existingEmployeeNewAppointment($json,$mysqli){
+
+}
+function existingEmployeeUpdateAppointment($json,$mysqli){
+
+}
+
 
 function getEmployees($mysqli){
     $data = [];
