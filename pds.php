7be4e9d40f343fbe6f73f1d93d5f8b0b6990a8b0
Franz Joshua A Valencia <franzval123@gmail.com>
2020-05-27 15:08:59 +0800

    wip

--- pds.php
+++ pds.php
@@ -1,61 +1,403 @@
-<?php require 'header.php'?>
-<div class="ui container segment">
-<!-- component start -->
-<h4>I. PERSONAL INFORMATION</h4>
-<hr>
-<table>
-<tr>
-    <td>
-<div class="ui left labeled mini input">
-    <div class="ui label">
-        Lastname:
+
+<div class="">
+    <!-- component start -->
+    <h4>I. PERSONAL INFORMATION</h4>
+    <hr>
+    <div class="ui grid">
+        <div class="eight wide column">
+            <i>Bio:</i>
+            <hr>
+            <div class="ui form">
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Lastname:
+                            </div>
+                            <input name="last_name" type="text" placeholder="Enter lastname...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Firstname:
+                            </div>
+                            <input name="first_name" type="text" placeholder="Enter firstname...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Middlename:
+                            </div>
+                            <input name="middle_name" type="text" placeholder="Enter middlename...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Name ext.:
+                            </div>
+                            <input name="extName" type="text" placeholder="Enter name extension...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Date of Birth:
+                            </div>
+                            <input name="birthdate" type="date" placeholder="Enter birthdate...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Place of Birth:
+                            </div>
+                            <input name="place_of_birth" type="text" placeholder="Enter birthplace...">
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="fields">
+                    <div class="eight wide field">
+
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Sex:
+                            </div>
+                            <input name="gender" type="text" placeholder="Gender...">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Civil Status:
+                            </div>
+                            <input name="civil_status" type="text" placeholder="Civil status...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+
+
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Height (m):
+                            </div>
+                            <input name="height" type="text" placeholder="Height...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Weight (kg):
+                            </div>
+                            <input name="weight" type="text" placeholder="Weight...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Blood Type:
+                            </div>
+                            <input name="blood_ty   pe" type="text" placeholder="Blood type...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <i>Government Agency Details</i>
+            <hr>
+            <div class="ui form">
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                GSIS ID No:
+                            </div>
+                            <input name="gsis" type="text" placeholder="GSIS ID...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                PAG-IBIG ID No:
+                            </div>
+                            <input name="pagibig" type="text" placeholder="PAG-IBIG ID...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                PHILHEALTH No:
+                            </div>
+                            <input name="philhealth" type="text" placeholder="PHILHEALTH No...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                SSS No:
+                            </div>
+                            <input name="sss" type="text" placeholder="SSS No...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                TIN No:
+                            </div>
+                            <input name="tin" type="text" placeholder="TIN No...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Agency Employee No:
+                            </div>
+                            <input name="agency_employee_no" type="text" placeholder="Agency Employee No...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Citizenship:
+                            </div>
+                            <input name="citizenship" type="text" placeholder="Citizenship...">
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+        <div class="eight wide column">
+            <i>Residential Address:</i>
+            <hr>
+            <div class="ui form">
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                House/Block/Lot No:
+                            </div>
+                            <input name="house_no" type="text" placeholder="House No...">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Street:
+                            </div>
+                            <input name="street" type="text" placeholder="Street...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Subdivision/Village:
+                            </div>
+                            <input name="subdvision" type="text" placeholder="Subdivision/Village...">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Barangay:
+                            </div>
+                            <input name="barangay" type="text" placeholder="Barangay...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                City/Municipality:
+                            </div>
+                            <input name="city" type="text" placeholder="City/Municipality...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Province:
+                            </div>
+                            <input name="province" type="text" placeholder="Province...">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <i>Permanent Address:</i>
+            <hr>
+            <div class="ui form">
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                House/Block/Lot No:
+                            </div>
+                            <input name="house_no" type="text" placeholder="House No...">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Street:
+                            </div>
+                            <input name="street" type="text" placeholder="Street...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Subdivision/Village:
+                            </div>
+                            <input name="subdvision" type="text" placeholder="Subdivision/Village...">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Barangay:
+                            </div>
+                            <input name="barangay" type="text" placeholder="Barangay...">
+                        </div>
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                City/Municipality:
+                            </div>
+                            <input name="city" type="text" placeholder="City/Municipality...">
+                        </div>
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Province:
+                            </div>
+                            <input name="province" type="text" placeholder="Province...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <i>Contact Details:</i>
+            <hr>
+            <div class="ui form">
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Telephone:
+                            </div>
+                            <input name="tel" type="text" placeholder="eg.(123)-456-6789">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Mobile:
+                            </div>
+                            <input name="mobile" type="text" placeholder="eg.09123456789">
+                        </div>
+
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+                        <div class="ui left labeled mini input">
+                            <div class="ui label">
+                                Email:
+                            </div>
+                            <input name="email" type="text" placeholder="Email...">
+                        </div>
+
+                    </div>
+                    <div class="eight wide field">
+
+                    </div>
+                </div>
+                <div class="fields">
+                    <div class="eight wide field">
+
+                    </div>
+                    <div class="eight wide field">
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
-    <input name="last_name" type="text" placeholder="Enter lastname...">
-</div>
-<div class="ui left labeled mini input">
+
+
+
+
+    <!-- <div class="ui left labeled mini input">
     <div class="ui label">
-        Firstname:
+        Province:
     </div>
-    <input name="first_name" type="text" placeholder="Enter firstname...">
-</div>
-<div class="ui left labeled mini input">
-    <div class="ui label">
-        Middlename:
-    </div>
-    <input name="middle_name" type="text" placeholder="Enter middlename...">
-</div>
-<div class="ui left labeled mini input">
-    <div class="ui label">
-        Name ext.:
-    </div>
-    <input name="extName" type="text" placeholder="Enter name extension...">
-</div>
-    </td>
-</tr>
-<tr>
-    <td>
-<div class="ui left labeled mini input">
-    <div class="ui label">
-        Date of Birth:
-    </div>
-    <input name="birthdate" type="date" placeholder="Enter birthdate...">
-</div>
-<div class="ui left labeled mini input">
-    <div class="ui label">
-        Place of Birth:
-    </div>
-    <input name="place_of_birth" type="text" placeholder="Enter birthplace...">
-</div>
-<div class="ui left labeled mini input">
-    <div class="ui label">
-        Sex:
-    </div>
-    <input name="gender" type="text" placeholder="Gender...">
+    <input name="province" type="text" placeholder="Province...">
+</div> -->
+    <script src="pds/config.js"></script>
+    <!-- component end -->
 </div>
 
-    </td>
-</tr>
-</table>
-<script src="pds/config.js"></script>
-<!-- component end -->
-</div>
