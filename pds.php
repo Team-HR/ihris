44635b482885d78749d3e0caf5e1dc7619e97572
Franz Joshua A Valencia <franzval123@gmail.com>
2020-05-27 13:14:48 +0800

    wip

---
+++ pds.php
@@ -0,0 +1,61 @@
+<?php require 'header.php'?>
+<div class="ui container segment">
+<!-- component start -->
+<h4>I. PERSONAL INFORMATION</h4>
+<hr>
+<table>
+<tr>
+    <td>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Lastname:
+    </div>
+    <input name="last_name" type="text" placeholder="Enter lastname...">
+</div>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Firstname:
+    </div>
+    <input name="first_name" type="text" placeholder="Enter firstname...">
+</div>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Middlename:
+    </div>
+    <input name="middle_name" type="text" placeholder="Enter middlename...">
+</div>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Name ext.:
+    </div>
+    <input name="extName" type="text" placeholder="Enter name extension...">
+</div>
+    </td>
+</tr>
+<tr>
+    <td>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Date of Birth:
+    </div>
+    <input name="birthdate" type="date" placeholder="Enter birthdate...">
+</div>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Place of Birth:
+    </div>
+    <input name="place_of_birth" type="text" placeholder="Enter birthplace...">
+</div>
+<div class="ui left labeled mini input">
+    <div class="ui label">
+        Sex:
+    </div>
+    <input name="gender" type="text" placeholder="Gender...">
+</div>
+
+    </td>
+</tr>
+</table>
+<script src="pds/config.js"></script>
+<!-- component end -->
+</div>
