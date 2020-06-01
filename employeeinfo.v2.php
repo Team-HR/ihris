56c635b1c4a6c9c6f2954a8102b9d5ba10c7cd72
franz <franzval123@gmail.com>
2020-06-01 11:08:56 +0800

    wip

--- employeeinfo.v2.php
+++ employeeinfo.v2.php
@@ -330,7 +330,68 @@ if (isset($_GET["spms"])) {
 	</div>
 
 	<div class="ui container" style="padding: 5px;">
-		<?php require 'pds.php'?>
+	
+<div class="ui segment grid" id="pds-app">
+  <div class="three wide column">
+    <div id="pim-menu" class="ui secondary vertical pointing menu fluid">
+      <a class="item" data-tab="appointments">
+        Appointment
+      </a>
+      <a class="item active" data-tab="pds">
+        PDS
+      </a>
+      <a class="item" data-tab="service_records">
+        Service Records
+      </a>
+      <a class="item" data-tab="leave_records">
+        Leave Records
+      </a>
+    </div>
+  </div>
+  <div class="thirteen wide stretched column">
+    <div class="ui tab" data-tab="appointments"></div>
+    <div class="ui tab active" data-tab="pds">
+      <div class="ui pointing secondary blue menu fluid" id="pds">
+        <a class="item active" data-tab="personal">Personal</a>
+        <a class="item" data-tab="family">Family</a>
+        <a class="item" data-tab="education">Education</a>
+        <a class="item" data-tab="eligibility">Eligibility</a>
+        <a class="item" data-tab="work_experience">Work Experiences</a>
+        <a class="item" data-tab="voluntary_works">Voluntary Works</a>
+        <a class="item" data-tab="trainings">Trainings</a>
+        <a class="item" data-tab="other_information">Other Information</a>
+      </div>
+      <div class="ui tab segment active" data-tab="personal">
+        <?php require 'pds_personal.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="family">
+        <?php require 'pds_family.php';?>
+      </div>
+      <div class="ui tab segment" data-tab="education">
+        <?php require 'pds_education.php';?>
+      </div>
+      <div class="ui tab segment" data-tab="eligibility">
+        <?php require 'pds_eligibility.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="work_experience">
+        <?php require 'pds_work_experience.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="voluntary_works">
+        <?php require 'pds_voluntary.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="trainings">
+        <?php require 'pds_trainings.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="other_information">
+        <?php require 'pds_other_information.php'?>
+      </div>
+    </div>
+    <div class="ui tab" data-tab="service_records"></div>
+    <div class="ui tab" data-tab="leave_records"></div>
+  </div>
+</div>
+
+<script src="pds/config.js"></script>
 	</div>
 	
 
