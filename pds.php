913eb4e4fad7bba24d527af394edeb4dda9d1392
franz <franzval123@gmail.com>
2020-06-01 10:49:56 +0800

    wip

--- pds.php
+++ pds.php
@@ -1,4 +1,36 @@
-<div class="ui tiny form">
+
+<div class="ui segment grid" id="pds-app">
+  <div class="three wide column">
+    <div id="pim-menu" class="ui secondary vertical pointing menu fluid">
+      <a class="item" data-tab="appointments">
+        Appointment
+      </a>
+      <a class="item active" data-tab="pds">
+        PDS
+      </a>
+      <a class="item" data-tab="service_records">
+        Service Records
+      </a>
+      <a class="item" data-tab="leave_records">
+        Leave Records
+      </a>
+    </div>
+  </div>
+  <div class="thirteen wide stretched column">
+    <div class="ui tab" data-tab="appointments"></div>
+    <div class="ui tab active" data-tab="pds">
+      <div class="ui pointing secondary blue menu fluid" id="pds">
+        <a class="item" data-tab="personal">Personal</a>
+        <a class="item" data-tab="family">Family</a>
+        <a class="item" data-tab="education">Education</a>
+        <a class="item" data-tab="eligibility">Eligibility</a>
+        <a class="item" data-tab="work_experience">Work Experiences</a>
+        <a class="item" data-tab="voluntary_works">Voluntary Works</a>
+        <a class="item" data-tab="trainings">Trainings</a>
+        <a class="item active" data-tab="other_information">Other Information</a>
+      </div>
+      <div class="ui tab segment" data-tab="personal">
+      <div class="ui tiny form">
     <h4 class="ui header">I. PERSONAL INFORMATION</h4>
 
     <i>Bio</i>
@@ -167,3 +199,32 @@
         </div>
     </div>
 </div>
+      </div>
+      <div class="ui tab segment" data-tab="family">
+        <?php require 'pds_family.php';?>
+      </div>
+      <div class="ui tab segment" data-tab="education">
+        <?php require 'pds_education.php';?>
+      </div>
+      <div class="ui tab segment" data-tab="eligibility">
+        <?php require 'pds_eligibility.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="work_experience">
+        <?php require 'pds_work_experience.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="voluntary_works">
+        <?php require 'pds_voluntary.php'?>
+      </div>
+      <div class="ui tab segment" data-tab="trainings">
+        <?php require 'pds_trainings.php'?>
+      </div>
+      <div class="ui tab segment active" data-tab="other_information">
+        <?php require 'pds_other_information.php'?>
+      </div>
+    </div>
+    <div class="ui tab" data-tab="service_records"></div>
+    <div class="ui tab" data-tab="leave_records"></div>
+  </div>
+</div>
+
+<script src="pds/config.js"></script>
