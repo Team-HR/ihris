05664e3d7ae4c6210ef6a4e327d8ab3bfc357248
franz <franzval123@gmail.com>
2020-06-01 09:42:55 +0800

    wip

---
+++ pds_other_information.php
@@ -0,0 +1,20 @@
+<div class="ui tiny form">
+    <h4 class="ui header">VIII. OTHER INFORMATION</h4>
+    <hr>
+    <table class="ui very small compact structured celled table">
+        <thead>
+        <tr class="center aligned">
+            <th>Special Skills/ Hobbies</th>
+            <th>Non-Academic Distinctions/ Recognition</th>
+            <th>Membership in Association/ Organization</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
---
+++ pds_trainings.php
@@ -0,0 +1,27 @@
+<div class="ui tiny form">
+    <h4 class="ui header">VII. TRAININGS</h4>
+    <hr>
+    <table class="ui very small compact structured celled table">
+        <thead>
+        <tr class="center aligned">
+            <th rowspan="2">Title of Seminar/ Conference/ Workshop/ Short Courses</th>
+            <th colspan="2">Inclusive Dates</th>
+            <th rowspan="2">Number of Hours</th>
+            <th rowspan="2">Conducted/ Sponsored By</th>
+        </tr>
+        <tr>
+            <th>From</th>
+            <th>To</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
