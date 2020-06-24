ef5bdd82d66cd5d29017094cdbd88c6737fbb36f
franz <franzval123@gmail.com>
2020-06-24 09:12:39 +0800

    Added phpoffice/phpspreadsheet with test.php as sample.

--- test.php
+++ test.php
@@ -1,163 +1,53 @@
 <?php
-require_once '_connect.db.php';
-// $data = array();
-// $sql = <<<SQL
 
-//     SELECT
-//         employees.employees_id,
-//         UPPER(CONCAT( employees.lastName, ', ', employees.firstName, ' ', employees.middleName, ' ', employees.extName )) AS fullName,
-//         CONCAT_WS(', ',prr.period, prr.year) AS period,
-//         prr.type,
-//         prrlist.comments 
-//     FROM
-//         employees    
-//     LEFT JOIN prrlist ON employees.employees_id = prrlist.employees_id
-//     LEFT JOIN prr ON prrlist.prr_id = prr.prr_id
-//     WHERE
-//         employees.`status` = 'ACTIVE' 
-//         -- AND prrlist.comments <> '' 
-//     ORDER BY
-//         employees.lastName ASC,
-//         prr.period DESC
-
-// SQL;
-
-// $result = $mysqli->query($sql);
-// while($row = $result->fetch_assoc()){
-//     $id = $row['employees_id'];
-//     $prr = [
-//         'period' => $row['period'],
-//         'type' => $row['type'],
-//         'comments' => $row['comments']
-//     ];
-
-//     if (!array_key_exists('id_'.$id,$data)){
-//         $data['id_'.$id] = [
-//         'id' => $id,
-//         'fullName' => $row['fullName'],
-//         'prr' => [$prr]
-//         ];
-//     } else $data['id_'.$id]['prr'][] = $prr;
-// }
-
-// print("<pre>".print_r($data,true)."</pre>");
-
-// $sql = <<< SQL
-// SELECT
-// 	COUNT(personneltrainings_id) AS num_trainings,
-// -- 	startDate,
-// 	MONTH(startDate) AS month,
-// 	YEAR(startDate) AS year
-// FROM
-// 	personneltrainings
-// GROUP BY
-// 	MONTH(startDate)
-// ORDER BY
-// -- MONTH(startDate) ASC,	
-// YEAR(startDate) ASC
-// SQL;
-
-// $data = [];
-// $res = $mysqli->query($sql);
-
-// while ($row = $res->fetch_assoc()) {
-// 	$year = $row['year'];
-// 	$months = [
-// 		'month' => $row['month'],
-// 		'num_trainings' => $row['num_trainings']
-// 	];
-
-// 	if (!array_key_exists('year_'.$year, $data)) {
-// 		$data['year_'.$year] = [
-// 			'year'	=> $year,
-// 			'months' => [
-// 				$months['month'] => $months
-// 			]
-// 		];
-// 	} else $data['year_'.$year]['months'][$months['month']] = $months;
-// }
-
-// echo json_encode($data);
-// print("<pre>".print_r($data,true)."</pre>");
-
-// $data = [];
-// $sql = <<<SQL
-// SELECT `competency`.*,`employees`.*,`positiontitles`.* FROM `competency` LEFT JOIN `employees` ON `competency`.`employees_id` = `employees`.`employees_id` LEFT JOIN `positiontitles` ON `employees`.`position_id` = `positiontitles`.`position_id`
-// WHERE
-// `category` = 'Administrative'
-// AND
-// `gender` = 'FEMALE'
-// SQL;
-// $filter = "";
-// echo "Filter: ".($filter?$filter:'N/A')."<br>";
+require "vendor/autoload.php";
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 
-$data = [];
-$filters = [
-    'category' => ['Key Position', 'Administrative', 'Technical'],
-    'nature_of_assignment' => ['RANK & FILE', 'SUPERVISORY'],
-    'gender' => ['MALE','FEMALE']
-];
+// Create new Spreadsheet object
+$spreadsheet = new Spreadsheet();
 
-$filters = [
-    ['Key Position', 'RANK & FILE', 'MALE'],
-    ['Key Position', 'RANK & FILE', 'FEMALE'],
-    ['Key Position', 'SUPERVISORY', 'MALE'],
-    ['Key Position', 'SUPERVISORY', 'FEMALE'],
-    ['Administrative', 'RANK & FILE', 'MALE'],
-    ['Administrative', 'RANK & FILE', 'FEMALE'],
-    ['Administrative', 'SUPERVISORY', 'MALE'],
-    ['Administrative', 'SUPERVISORY', 'FEMALE'],    
-    ['Technical', 'RANK & FILE', 'MALE'],
-    ['Technical', 'RANK & FILE', 'FEMALE'],
-    ['Technical', 'SUPERVISORY', 'MALE'],
-    ['Technical', 'SUPERVISORY', 'FEMALE']
-];
+// Set document properties
+$spreadsheet->getProperties()->setCreator('Maarten Balliauw')
+    ->setLastModifiedBy('Maarten Balliauw')
+    ->setTitle('Office 2007 XLSX Test Document')
+    ->setSubject('Office 2007 XLSX Test Document')
+    ->setDescription('Test document for Office 2007 XLSX, generated using PHP classes.')
+    ->setKeywords('office 2007 openxml php')
+    ->setCategory('Test result file');
 
-// print("<pre>".print_r($filters,true)."</pre>");
+// Add some data
+$spreadsheet->setActiveSheetIndex(0)
+    ->setCellValue('A1', 'Hello')
+    ->setCellValue('B2', 'world!')
+    ->setCellValue('C1', 'Hello')
+    ->setCellValue('D2', 'world!');
 
-$queries = [];
-foreach ($filters as $filter) {
-    $sql = "WHERE category = '$filter[0]' AND natureOfAssignment = '$filter[1]' AND gender = '$filter[2]'";
-    $queries[] = $sql;
-}
+// Miscellaneous glyphs, UTF-8
+$spreadsheet->setActiveSheetIndex(0)
+    ->setCellValue('A4', 'Miscellaneous glyphs')
+    ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
 
-// print("<pre>".print_r($queries,true)."</pre>");
+// Rename worksheet
+$spreadsheet->getActiveSheet()->setTitle('Simple');
 
-$sql_ave = "SELECT  ROUND(AVG(`competency`.`Adaptability`)) 'adaptability', ROUND(AVG(`competency`.`ContinousLearning`)) 'continuous_learning', ROUND(AVG(`competency`.`Communication`)) 'communication', ROUND(AVG(`competency`.`OrganizationalAwareness`)) 'organizational_awareness', ROUND(AVG(`competency`.`CreativeThinking`)) 'creative_thinking', ROUND(AVG(`competency`.`NetworkingRelationshipBuilding`)) 'networking_relationship_building', ROUND(AVG(`competency`.`ConflictManagement`)) 'conflict_management', ROUND(AVG(`competency`.`StewardshipofResources`)) 'stewardship_of_resources', ROUND(AVG(`competency`.`RiskManagement`)) 'risk_management', ROUND(AVG(`competency`.`StressManagement`)) 'stress_management', ROUND(AVG(`competency`.`Influence`)) 'influence', ROUND(AVG(`competency`.`Initiative`)) 'initiative', ROUND(AVG(`competency`.`TeamLeadership`)) 'team_leadership', ROUND(AVG(`competency`.`ChangeLeadership`)) 'change_leadership', ROUND(AVG(`competency`.`ClientFocus`)) 'client_focus', ROUND(AVG(`competency`.`Partnering`)) 'partnering', ROUND(AVG(`competency`.`DevelopingOthers`)) 'developing_others', ROUND(AVG(`competency`.`PlanningandOrganizing`)) 'planning_and_organizing', ROUND(AVG(`competency`.`DecisionMaking`)) 'decision_making', ROUND(AVG(`competency`.`AnalyticalThinking`)) 'analytical_thinking', ROUND(AVG(`competency`.`ResultsOrientation`)) 'results_orientation', ROUND(AVG(`competency`.`Teamwork`)) 'teamwork', ROUND(AVG(`competency`.`ValuesandEthics`)) 'values_and_ethics', ROUND(AVG(`competency`.`VisioningandStrategicDirection`)) 'visioning_and_strategic_direction' FROM `competency` LEFT JOIN `employees` ON `competency`.`employees_id` = `employees`.`employees_id` LEFT JOIN `positiontitles` ON `employees`.`position_id` = `positiontitles`.`position_id`";
+// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+$spreadsheet->setActiveSheetIndex(0);
 
-// print("<pre>".print_r($wheres,true)."</pre>");
-$sql_emp = "SELECT CONCAT(`employees`.`lastName`,', ',`employees`.`firstName`,' ',`employees`.`middleName`,' ',`employees`.`extName`) AS fullName FROM `competency` LEFT JOIN `employees` ON `competency`.`employees_id` = `employees`.`employees_id` LEFT JOIN `positiontitles` ON `employees`.`position_id` = `positiontitles`.`position_id`";
+// Redirect output to a client’s web browser (Xlsx)
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="01simple.xlsx"');
+header('Cache-Control: max-age=0');
+// If you're serving to IE 9, then the following may be needed
+header('Cache-Control: max-age=1');
 
-foreach ($queries as $qk => $qry) {
-    $key = $filters[$qk][0].";".$filters[$qk][1];
-    // echo $key."<br>";
+// If you're serving to IE over SSL, then the following may be needed
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+header('Pragma: public'); // HTTP/1.0
 
-
-    //get the average competency
-    $result = $mysqli->query($sql_ave.$qry);
-    $data_fltrs = [$filters[$qk][0],$filters[$qk][1]];
-    $data_ave = $result->fetch_assoc();
-    // now get the list of employees
-    $data_emps = [];
-    $result = $mysqli->query($sql_emp.$qry." ORDER BY `employees`.`lastName` ASC");
-    while ($emp = $result->fetch_assoc()) {
-        $data_emps[] = $emp['fullName'];   
-    }
-
-    if (!array_key_exists($key, $data)) {
-        //male
-        $data[$key]['filters'] = $data_fltrs;
-        $data[$key]['male'] = [
-            'average' => $data_ave,
-            'employees' => $data_emps
-        ];
-	} else {
-        $data[$key]['female'] = [
-            'average' => $data_ave,
-            'employees' => $data_emps
-        ];
-    }
-}
-
-print("<pre>".print_r($data,true)."</pre>");
+$writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+$writer->save('php://output');
+exit;
