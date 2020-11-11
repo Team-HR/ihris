8a6f4cc42f51d05cc7704463574f054a9a6fcb8b
fjavalencia <franzval123@gmail.com>
2020-11-11 16:27:23 +0800

    Feat: HR can now add, edit or delete awards either in personnel recognition and PDS other information tab per employee. (Needs evaluation from Maam Tessie)

--- personnelrecognition_proc.php
+++ personnelrecognition_proc.php
@@ -3,107 +3,87 @@ require '_connect.db.php';
 require 'libs/NameFormatter.php';
 // $nameFormatter = new NameFormatter;
 
-if (isset($_POST['loadlist'])){
-	$sql = "SELECT `employees`.`employees_id` AS `emp_id`,`firstName`,`lastName`,`middleName`,`extName`,`department`,`position`,`recognition` FROM `employees` LEFT JOIN `department` ON `employees`.`department_id` = `department`.`department_id` LEFT JOIN `positiontitles` ON `employees`.`position_id` = `positiontitles`.`position_id` LEFT JOIN `rnr_recognitions` ON `employees`.`employees_id` = `rnr_recognitions`.`employees_id` ORDER BY `lastName` ASC";
-	$result = $mysqli->query($sql);
-	while ($row = $result->fetch_assoc()) {	
-		$employees_id = $row["emp_id"];
-		$nameFormatter = new NameFormatter($row["firstName"],$row["lastName"],$row["middleName"],$row["extName"]);
-		$fullName = $nameFormatter->getFullname();
-		// // sql insertion starts here
-		// 	$sql1 = "INSERT INTO `rnr_recognitions` (`recognition_id`, `employees_id`, `recognition`, `timestamp`) VALUES (NULL, '$employees_id', NULL, CURRENT_TIMESTAMP)";
-		// 	$mysqli->query($sql1);
-		// // sql insertion ends here
-		
-?>
-<tr style="">
-	<!-- <td><?=var_dump($row)?></td> -->
-	<td class=" center aligned"><?=str_pad($employees_id,5,0,STR_PAD_LEFT);?></td>
-	<td style="width: 85px;">	
-		
-				<div class="ui icon green mini buttons">
-					<!-- <button onclick="viewFunc('<?php echo $controlNumber;?>')" title="Open" class="ui button"><i class="folder open outline icon"></i></button> -->
-					<button onclick="updateFunc('<?=$employees_id?>','<?=$fullName?>')" title="Quick Edit" class="ui button"><i class="edit outline icon"></i> Update</button>
-					<!-- <button onclick="deleteFunc('<?php echo $controlNumber;?>')" title="Delete" class="ui button"><i class="trash alternate outline icon"></i></button> -->
-				</div>
+if (isset($_POST['get_data'])) {
+    // $sql = "SELECT `employees`.`employees_id` AS `emp_id`,`firstName`,`lastName`,`middleName`,`extName`,`department`,`position`,`recognition` FROM `employees` LEFT JOIN `department` ON `employees`.`department_id` = `department`.`department_id` LEFT JOIN `positiontitles` ON `employees`.`position_id` = `positiontitles`.`position_id` LEFT JOIN `rnr_recognitions` ON `employees`.`employees_id` = `rnr_recognitions`.`employees_id` ORDER BY `lastName` ASC";
+    $sql = "SELECT `employees`.`employees_id` AS `emp_id`,`firstName`,`lastName`,`middleName`,`extName`,`department`,`position`,`recognition` FROM `employees` LEFT JOIN `department` ON `employees`.`department_id` = `department`.`department_id` LEFT JOIN `positiontitles` ON `employees`.`position_id` = `positiontitles`.`position_id` LEFT JOIN `rnr_recognitions` ON `employees`.`employees_id` = `rnr_recognitions`.`employees_id` ORDER BY `lastName` ASC LIMIT 50";
+    $result = $mysqli->query($sql);
+    $data = [];
 
-	</td>
-	<td><?=$fullName?></td>
+    while ($row = $result->fetch_assoc()) {
+        $employees_id = $row["emp_id"];
+        $nameFormatter = new NameFormatter($row["firstName"], $row["lastName"], $row["middleName"], $row["extName"]);
+        $row['fullName'] = mb_convert_case($nameFormatter->getFullname(), MB_CASE_UPPER, "UTF-8");
+        // $row;
 
-	<td style="min-width: 500px;">
-		<!-- Recognitions here -->
-		<?php
-		$recognition = $row["recognition"];	
-		// $recognition = "<span style='white-space: pre;'>".$row["recognition"]."</span>";	
-		if (!$recognition) {
-			// $recognition = "<span style='color:grey'>N/A</span>";
-			$recognition = "<i style='color:grey'>N/A</i>";
-		} else {
-			// $recognition = "<span style='white-space: pre;'>".$recognition."<span>";
-			$recogs_arr = unserialize($recognition);
-			$recognition="";
-			foreach ($recogs_arr as $key => $value) {
-				$recognition .= ($key+1).". ".$value."<br>";
-			}
-		}
-		echo $recognition;
-		?>
-	</td>
+        $row["recs"] = get_awards($employees_id, $mysqli);
 
-	<td><?php
-	$position = $row["position"];
-		if (!$position) {
-			$position = "<i style='color:grey'>N/A</i>";
-		}
-		echo $position;
-	?></td>
+        $data[] = $row;
+    }
 
-	<td><?php
-	$department = $row["department"];
-		if (!$department) {
-			$department = "<i style='color:grey'>N/A</i>";
-		}
-		echo $department;
-	?></td>
+    echo json_encode($data);
+} elseif (isset($_POST["loadEntry"])) {
+    $employees_id = $_POST["employees_id"];
+    $sql = "SELECT * FROM `rnr_recognitions` WHERE `employees_id` = '$employees_id'";
+    $result = $mysqli->query($sql);
+    $num_rows = $result->num_rows;
+    $data = array();
+    if ($num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $data = unserialize($row["recognition"]);
+    } else {
+        $recognition = serialize($data);
+        if ($stmt = $mysqli->prepare("INSERT INTO `rnr_recognitions` (`recognition_id`, `employees_id`, `recognition`, `timestamp`) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP)")) {
+            $stmt->bind_param("is", $employees_id, $recognition);
+            $stmt->execute();
+            $stmt->close();
+        }
+    }
+    echo json_encode($data);
+} elseif (isset($_POST["updateEntry"])) {
+    $employee_id = $_POST["employees_id"];
+    $recs =  $_POST["recs"];
+    if (count($recs) == 0) {
+        $sql = "DELETE FROM `pds_non_academic_recognitions` WHERE `pds_non_academic_recognitions`.`employee_id` = $employee_id";
+        $mysqli->query($sql);
+        // $mysqli->close();
+        return false;
+    }
+    
+    // if ($stmt = $mysqli->prepare("UPDATE `rnr_recognitions` SET `recognition`=? WHERE `employees_id`=?")) {
+    //     $stmt->bind_param("si", $recognition, $employees_id);
+    //     $stmt->execute();
+    //     $stmt->close();
+    // }
+    $employee_id = $mysqli->real_escape_string($employee_id);
+    $sql = "DELETE FROM `pds_non_academic_recognitions` WHERE `pds_non_academic_recognitions`.`employee_id` = $employee_id";
+    $mysqli->query($sql);
+    // $mysqli->close();
+    // echo json_encode($mysqli->error);
 
-
-
-
-</tr>
-
-<?php
-	}
+    foreach ($recs as $rec) {
+        $rec = $mysqli->real_escape_string($rec);
+        $sql = "INSERT INTO `pds_non_academic_recognitions` (`id`,`employee_id`, `nar_title`) VALUES (NULL,$employee_id, '$rec')";
+        $mysqli->query($sql);
+    }
+    // $mysqli->close();
+    
+    // echo json_encode($mysqli->error);
 }
 
-elseif (isset($_POST["loadEntry"])) {
-	$employees_id = $_POST["employees_id"];
-	$sql = "SELECT * FROM `rnr_recognitions` WHERE `employees_id` = '$employees_id'";
-	$result = $mysqli->query($sql);
-	$num_rows = $result->num_rows;
-	$data = array();
-	if ($num_rows > 0) {
-		$row = $result->fetch_assoc();
-		$data = unserialize($row["recognition"]);
-	} else {
-		$recognition = serialize($data);
-		if ($stmt = $mysqli->prepare("INSERT INTO `rnr_recognitions` (`recognition_id`, `employees_id`, `recognition`, `timestamp`) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP)")) {
-			$stmt->bind_param("is",$employees_id,$recognition);
-			$stmt->execute();
-			$stmt->close();
-		}
-	}
-	echo json_encode($data);
+function get_awards($employees_id, $mysqli)
+{
+    $data = [];
+    if (!$employees_id) return $data;
+    $employees_id = $mysqli->real_escape_string($employees_id);
+
+    $sql = "SELECT * FROM `pds_non_academic_recognitions` WHERE `employee_id` = $employees_id";
+
+    $result = $mysqli->query($sql);
+
+    while ($row = $result->fetch_assoc()) {
+        $datum = $row['nar_title'];
+        $data[] = $datum;
+    }
+
+    return $data;
 }
-
-elseif (isset($_POST["updateEntry"])) {
-	$employees_id = $_POST["employees_id"];
-	$recognition =  serialize($_POST["recognition"]);
-	if ($stmt = $mysqli->prepare("UPDATE `rnr_recognitions` SET `recognition`=? WHERE `employees_id`=?")) {
-		$stmt->bind_param("si",$recognition,$employees_id);
-		$stmt->execute();
-		$stmt->close();
-	}
-
-}
-
-?>
