e4bf2afffdd7a6267691a61f3e9b6273e212c726
Vanessa <arochavanessa28@gmail.com>
2020-11-20 11:29:12 +0800

    wop

--- umbra/leaveManagement/config.php
+++ umbra/leaveManagement/config.php
@@ -10,8 +10,21 @@
         }
         echo json_encode($a);
     }elseif (isset($_POST['saveLeave'])) {
-    }
-?>
+        $emp_id = $_POST["emp_id"];
+        $date_received = $_POST["date_received"];
+        $leaveType = $_POST["leaveType"];
+        $sp_type = $_POST["sp_type"];
+        $remarks = $_POST["remarks"];
+        $selectedDate = $_POST["selectedDate"];
+        $controlNumber = $mysqli->insert_id;
 
+        $sql = "INSERT INTO `lm_logs` (`log_id`, `controlNumber`, `employees_id`, `dateReceived`, `dateApplied`, `totalDays`, `leaveType` , `sp_type`, `status`, `remarks`)
+             VALUES ('', '', '$emp_id', '$date_received', '$selectedDate', '', '$leaveType', '$sp_type', '', '$remarks')";
+            $sql = $mysqli->query($sql);
+            }
+
+        
+?>
+ 
 
 
