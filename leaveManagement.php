a61219ea022defdef0b83592280a8303e7c64931
Vanessa <arochavanessa28@gmail.com>
2020-11-24 15:09:46 +0800

    Nov.24-updated wip van

--- leaveManagement.php
+++ leaveManagement.php
@@ -116,7 +116,147 @@
         <div class="ui button cancel negative">Close</div>
       </div>
     </div>
-    <!--   add modal End   -->
+<!--   add modal End   -->
+
+<!--    edit model    -->
+ <div class="ui modal small" id="editModal">
+      <div class="header">
+        <h3 class="ui header">Edit Leave Record</h3>
+      </div>
+      <div class="content">
+        <div class="ui form">
+          <div class="field">
+            <div class="two fields">
+              <div class="field">
+                <label>Control #:</label>
+                <input type="text" name="" readonly value="HRMO-LEAVE<?=DATE('Y')?>-*****">
+              </div>
+              <div class="field">
+                <label>Date Received:</label>
+                <input type="date" name=""> 
+              </div>
+            </div>
+          </div>
+          
+          <div class="field">
+            <div class="two fields">
+              <div class="field">
+                <label>Name:</label>
+                <select class="ui fluid search dropdown" >
+                    <option value="">Employee</option>
+                    <template>
+                      <option v-for="(employee,index) in Employees" :value="employee.employees_id">{{employee.lastName}}, {{employee.firstName}}</option>
+                    </template>
+                </select>
+              </div>
+              <div class="field">
+                <label>Type of leave:</label>
+                <select class="ui fluid search dropdown" >
+                    <option value="">Type</option>
+                    <option value="VL">Vacation Leave</option>
+                    <option value="FL">Forced Leave</option>
+                    <option value="SL">Sick Leave</option>
+                    <option value="SP">Special Leave</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <template  v-if="leaveType=='SP'">
+            <div class="field">
+                <label>Type of Special Leave:</label>
+                <select class="ui fluid search dropdown" >
+                    <option value="Filial Leave">Filial Leave</option>
+                    <option value="SP">Domestic Emergency Leave</option>
+                    <option value="VL">Maternity Leave</option>
+                    <option value="FL">Rehabilitation Leave</option>
+                    <option value="SL">Solo Parent Leave</option>
+                    <option value="SP">Paternity Leave</option>
+                    <option value="SP">Magna Carta Leave</option>
+                    <option value="SP">Anti-VAWCY Leave</option>
+                    <option value="SP">Study Leave</option>
+                </select>
+          </div>
+          </template>
+          <div class="field">
+            <label>Applied Date:</label>
+            <form class="two fields" @submit.prevent="cal()">
+              <div class="field">
+                <input type="date" name="" id="calen" required>
+              </div>
+              <div class="field">
+                <input type="submit" class="ui button primary" value="ADD">
+              </div>
+            </form>
+          </div>
+          <div class="ui link list">
+            <a class="item" v-for="(d,index) in selectedDate" :key="index">
+              <span class="left floated like">
+                  {{d}}
+              </span>
+              <span class="right floated">
+                <i class="close icon" @click="slice_date(index)"></i>
+              </span>          
+            </a>
+          </div>
+          <br>
+          <div class="field">
+            <label>Remarks:</label>
+            <textarea width="100%" v-model="remarks"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="actions">
+        <div class="ui approve button positive" @click="saveLeave()">Save</div>
+        <div class="ui button cancel negative">Close</div>
+      </div>
+    </div>
+<!--   edit modal End   -->
+
+<!--    open model    -->
+ <div class="ui small modal" id="approveModal">
+      <div class="header">
+        <h3 class="ui header"> Approving Leave Application</h3>
+      </div>
+      <div class="content">
+        <div class="ui form">
+
+         <div class="two fields">
+              <div class="field">
+                <label>Control Number: HRMO-LEAVE-2020-0001</label>
+            </div>
+            <div class="field">
+                <strong>Date Received: February 30, 2021</strong>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="field">
+              <strong>Name of Applicant: Juan Dela Cruz</strong> 
+            </div>
+            <div class="field">
+              <strong>Type of Leave: Sick Leave</strong> 
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="field">
+              <strong>Name of Applicant: Juan Dela Cruz</strong> 
+            </div>
+            <div class="field">
+              <strong>Type of Leave: Sick Leave</strong> 
+            </div>
+          </div>
+
+        </div>
+        
+      </div>
+      <div class="actions">
+        <div class="ui approve button positive" @click="saveLeave()">Approve</div>
+        <div class="ui button cancel negative">Close</div>
+      </div>
+    </div>
+ <!--   open modal End   -->
+
     <div class="ui medium header">Leave Admin Dashboard</div>
     <button class="ui positive button" id="addButton" onclick="showAddModal()">Add New</button>
     <template v-if="Logs.length">
@@ -136,7 +276,8 @@
       </thead>
       <tbody>
         <tr v-for="(log,index) in Logs" :key="index">
-          <td>{{ log.controlNumber }}</td>
+          <td>{{ log.log_id }}
+          </td>
           <td>{{ log.dateReceived }}</td>
           <td>{{ log.firstName}} {{ log.lastName}}</td>
           <td>{{ log.dateApplied }}</td>
@@ -145,13 +286,13 @@
           <td>{{ log.remarks }}</td>
           <td>{{ log.status }}</td>
           <td>
-            <button class="circular ui icon button green">
+            <button class="circular ui icon button green" id="openButton" onclick="approveModal()">
               <i class="icon check"></i>
             </button>
             <button class="circular ui icon button red">
               <i class="icon close"></i>
             </button>
-            <button class="circular ui icon button blue">
+            <button class="circular ui icon button blue" id="editButton" onclick="showEditModal()">
               <i class="icon edit"></i>
             </button>
           </td>
@@ -167,6 +308,12 @@
 
     function showAddModal() {
       $("#addModal").modal('show');
+    }
+    function showEditModal() {
+      $("#editModal").modal('show');
+    }
+    function approveModal() {
+      $("#approveModal").modal('show');
     }
   </script>
   <script src="umbra/leaveManagement/leave.js"></script>
--- umbra/leaveManagement/config.php
+++ umbra/leaveManagement/config.php
@@ -17,11 +17,16 @@
         $remarks = $_POST["remarks"];
         $selectedDate = $_POST["selectedDate"];
         $totalDays = $_POST["totalDays"];
-        $controlNumber = $mysqli->insert_id;
+   
 
         $sql = "INSERT INTO `lm_logs` (`log_id`, `controlNumber`, `employees_id`, `dateReceived`, `dateApplied`, `totalDays`, `leaveType` , `sp_type`, `status`, `remarks`)
-             VALUES ('', '', '$emp_id', '$date_received', '$selectedDate', '$totalDays', '$leaveType', '$sp_type', '', '$remarks')";
+             VALUES (NULL, '', '$emp_id', '$date_received', '$selectedDate', '$totalDays', '$leaveType', '$sp_type', '', '$remarks')";
+            
             $sql = $mysqli->query($sql);
+        
+        $controlNumber = $mysqli->insert_id;
+
+       
     }elseif (isset($_POST['getLog'])) {
         $sql = "SELECT * from `lm_logs` left join `employees` on `lm_logs`.`employees_id`=`employees`.`employees_id`";
         $sql = $mysqli->query($sql);
