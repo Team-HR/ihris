c8e642bfd47dbcef054d4c96cbf2076c049e0888
fjavalencia <franzval123@gmail.com>
2021-02-19 12:32:17 +0800

    Added table of summaries

--- rnr_system_review.php
+++ rnr_system_review.php
@@ -4,7 +4,7 @@ require "header.php";
 ?>
 
 <div id="app" class="ui container">
-	<div class="ui basic segment">
+	<div class="ui segment">
 		<div class="ui borderless blue inverted mini menu">
 			<div class="left item" style="margin-right: 0px !important;">
 				<button onclick="window.history.back();" class="blue ui icon button" title="Back" style="width: 65px;">
@@ -18,6 +18,87 @@ require "header.php";
 
 		<!-- start here -->
 		<template>
+
+			<!-- <div class="two wide column"><canvas id="AdaptabilityBarChart" style="height: 50px;"></canvas></div> -->
+			<h1 class="ui header block">Total Respondents: {{data.length}}</h1>
+			<table class="ui compact table">
+				<thead>
+					<tr>
+						<th>Question</th>
+						<th>Yes</th>
+						<th>No</th>
+						<th>Preferred not to answer</th>
+					</tr>
+				</thead>
+				<tbody>
+					<template v-for="(qs, k) in questions">
+						<tr :key="k" v-if="k <= 5">
+							<td>{{qs}}</td>
+							<td>{{get_percentage(data2.datus[k].yes)}}</td>
+							<td>{{get_percentage(data2.datus[k].no)}}</td>
+							<td>{{get_percentage(data2.datus[k].no_answer)}}</td>
+						</tr>
+					</template>
+				</tbody>
+			</table>
+
+			<h3 class="ui header block">Effects on Performance</h3>
+
+
+			<!-- <div class="ui grid">
+				<div class="eight wide column"><canvas id="AdaptabilityBarChart" style="height: 50px;"></canvas></div>
+			</div> -->
+
+
+			<table class="ui compact table">
+				<thead>
+					<tr>
+						<th>Performance</th>
+						<th>Poor</th>
+						<th>Usatisfactory</th>
+						<th>Satisfactory</th>
+						<th>Very Satisfactory</th>
+						<th>Outstanding</th>
+					</tr>
+				</thead>
+				<tbody>
+					<template v-for="(perf, p) in performances">
+						<tr :key="p" >
+							<td>{{perf}}</td>
+							<td>{{get_percentage(data2.performances[p][1])}}</td>
+							<td>{{get_percentage(data2.performances[p][2])}}</td>
+							<td>{{get_percentage(data2.performances[p][3])}}</td>
+							<td>{{get_percentage(data2.performances[p][4])}}</td>
+							<td>{{get_percentage(data2.performances[p][5])}}</td>
+						</tr>
+					</template>
+				</tbody>
+			</table>
+
+			<h3 class="ui header block">Impacts on Employees</h3>
+			<table class="ui compact table">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Decreased</th>
+						<th>No Impact</th>
+						<th>Increased</th>
+					</tr>
+				</thead>
+				<tbody>
+					<template v-for="(perf, l) in impacts">
+						<tr :key="l">
+							<td>{{perf}}</td>
+							<td>{{get_percentage(data2.impacts[l][1])}}</td>
+							<td>{{get_percentage(data2.impacts[l][2])}}</td>
+							<td>{{get_percentage(data2.impacts[l][3])}}</td>
+						</tr>
+					</template>
+				</tbody>
+			</table>
+
+		<h2>Respondents:</h2>
+
 			<div class="ui two cards">
 				<div class="card" v-for="(item, index) in data" :key="index">
 					<div class="content">
@@ -28,7 +109,7 @@ require "header.php";
 								<div v-if="i <= 5">
 									<p v-if="i == 2 && item[1] == 'No' || i == 3 && item[1] == 'No'" :key="i"></p>
 									<p v-else-if="i == 4 && item[4] != 'Yes' || i == 5 && item[5] != 'Yes'" :key="i">
-									{{qs}} <b style="color: green;">No, {{item[i]}}</b>
+										{{qs}} <b style="color: green;">No, {{item[i]}}</b>
 									</p>
 									<p v-else :key="i">{{qs}} <b style="color: green;">{{item[i]}}</b></p>
 								</div>
@@ -176,6 +257,19 @@ require "header.php";
 		data() {
 			return {
 				data: [],
+				data2: [],
+				configs: [],
+				performances: [
+					"Dependability",
+					"Cooperation",
+					"Commitment",
+					"Initiative",
+				],
+				impacts: [
+					"Morale",
+					"Motivation",
+					"Productivity",
+				],
 				questions: [
 					"Are you aware of the Ordinance authorizing the grant and prescribing the guidelines of the exemplary Service Incentive?",
 					"Have you read the guidelines in the granting of incentive bonus?",
@@ -199,20 +293,129 @@ require "header.php";
 					},
 					"json"
 				);
+			},
+			get_data_summary() {
+				// $.post("rnr_system_review_proc.php", {
+				// 		get_data_summary: true,
+				// 	}, (data, textStatus, jqXHR) => {
+				// 		console.log(data)
+				// 		this.data2 = Object.assign([], data)
+				// 	},
+				// 	"json",
+				// 	async: false
+				// );
+				$.ajax({
+					type: "POST",
+					url: "rnr_system_review_proc.php",
+					data: {
+						get_data_summary: true
+					},
+					dataType: "json",
+					success: (response) => {
+						this.data2 = Object.assign([], response)
+						console.log(response);
+					},
+					async: false
+
+				});
+			},
+			get_percentage(num) {
+
+				if (num) {
+					var percent = ((num / this.data.length) * 100).toFixed(0)
+					return percent + "%"
+				}
+				return "0%"
+
 			}
 		},
 		created() {
 			this.get_data()
+			this.get_data_summary()
 		},
-		beforeMount() {},
 		mounted() {
+			var ctx2 = $("#AdaptabilityBarChart");
+
+			this.data2.performances.forEach((element, key) => {
+
+				var config2 = {
+					type: 'bar',
+					data: {
+						labels: [
+							"Poor ",
+							"Usatisfactory ",
+							"Satisfactory ",
+							"Very Satisfactory ",
+							"Outstanding "
+						],
+						datasets: [{
+							label: this.performances[0],
+							data: [3, 1, 3, 2, 5],
+							backgroundColor: [
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(75, 192, 192, 0.2)',
+								'rgba(54, 162, 235, 0.2)',
+								'rgba(153, 102, 255, 0.2)',
+
+							],
+							borderColor: [
+								'rgba(255, 99, 132, 1)',
+								'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+								'rgba(54, 162, 235, 1)',
+								'rgba(153, 102, 255, 1)',
+
+							],
+							fill: true,
+							borderWidth: 2,
+						}]
+					},
+					options: {
+						scales: {
+							yAxes: [{
+								scaleLabel: {
+									display: true,
+									labelString: 'Number of Personnels'
+								},
+								ticks: {
+									display: false,
+									beginAtZero: true,
+									// max: 100,
+									stepSize: 1
+								}
+							}],
+
+						}, //end of scales
+
+						title: {
+							display: true,
+							text: "Dependability",
+							fontSize: 20
+						},
+						legend: {
+							display: false,
+						},
+
+					},
+
+				};
+
+
+				this.configs.push = config2;
+			});
+
+
+
+			ldnLsaBarCharts = new Chart(ctx2, this.configs[0]);
 
 		},
+
 	})
+
 	$(document).ready(function() {
-		// $('.rating').rating({
-		// 	maxRating: 5
-		// });
+
+
 	});
 </script>
 
