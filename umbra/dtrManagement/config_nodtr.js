a4639840e5b7f4449b13253b1fe5e4816339a5b0
toto <japorms53@gmail.com>
2021-02-19 15:25:25 +0800

    nodtrpage complete

--- umbra/dtrManagement/config_nodtr.js
+++ umbra/dtrManagement/config_nodtr.js
@@ -20,25 +20,72 @@ var noDtr = new Vue({
                 }               
                 xml.open('POST','umbra/dtrManagement/config_nodtr.php',false);
                 xml.send(fd);
-            },
-            periodSearch:function(){
-                    this_dtr = this
-                var fd = new FormData() 
-                    fd.append('getNoDtr',this.period);
-                var xml = new XMLHttpRequest()
-                    xml.onload = function(){
-                        try {
-                            this_dtr.noDtrs  = JSON.parse(xml.responseText);
-                        } catch (error) {
-                            alert(error);
-                        }
+        },
+        periodSearch:function(){
+            searchPeriod = document.getElementById("searchPeriod");
+            searchPeriod.classList.add("loading","disabled");
+                $('body').toast({
+                    class: 'warning',
+                    message: `Fetching Please wait....`
+                });
+                this.noDtrs = [];
+                this_dtr = this
+            var fd = new FormData() 
+                fd.append('getNoDtr',this.period);
+            var xml = new XMLHttpRequest()
+                xml.onload = function(){
+                    $('body').toast({
+                        class: 'warning',
+                        message: `Checking for ERROR`
+                    });                       
+                    try {
+                        $('body').toast({
+                            class: 'warning',
+                            message: `Storing fetched Data`
+                        });                           
+                        this_dtr.noDtrs  = JSON.parse(xml.responseText);
+                        $('body').toast({
+                            class: 'success',
+                            message: `Complete`
+                        });                           
+                    } catch (error) {
+                        alert(error);
                     }
-                    xml.open('POST','umbra/dtrManagement/config_nodtr.php',false);
-                    xml.send(fd);
                 }
+                xml.open('POST','umbra/dtrManagement/config_nodtr.php',false);
+                xml.send(fd);
+                searchPeriod.classList.remove("loading","disabled");
+        },
+        submitted:function(i){
+            dat = this.noDtrs[i]
+            this_dtr = this
+            var fd = new FormData();
+                fd.append('dtrSubmitted',dat['dtrSummary_id']);
+                fd.append('employee_id',dat['employees_id']);
+                fd.append('period',this.period);
+            var xml = new XMLHttpRequest();
+                xml.onload = function(){
+                    this_dtr.periodSearch()
+                }
+                xml.open('POST','umbra/dtrManagement/config_nodtr.php',false)
+                xml.send(fd);
+        },
+        letterSent:function(i){
+            dat = this.noDtrs[i]
+            this_dtr = this
+            var fd = new FormData();
+                fd.append('letterSent',dat['dtrSummary_id']);
+                fd.append('employee_id',dat['employees_id']);
+                fd.append('period',this.period);
+            var xml = new XMLHttpRequest();
+                xml.onload = function(){
+                    this_dtr.periodSearch()
+                }
+                xml.open('POST','umbra/dtrManagement/config_nodtr.php',false)
+                xml.send(fd);
+        }
     },
     mounted:function(){
         this.getDepartments();
     }
-
 })
