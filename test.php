509c6d0464fb3f8f25369b66197f70b8a57b4250
fjavalencia <franzval123@gmail.com>
2021-09-03 15:26:12 +0800

    feat: experience now accepts year only

--- test.php
+++ test.php
@@ -1,23 +1,16 @@
 <?php
 
-require_once "libs/models/Competency.php";
-require_once "competency_requests.ajax.php";
-require_once "_connect.db.php";
+require "_connect.db.php";
 
-$competency = new Competency;
-$competencies = $competency->competencies;
+$sql = "SELECT * FROM `rsp_applicants` WHERE `applicant_id` = '334'";
+$result = $mysqli->query($sql);
+$row = $result->fetch_assoc();
 
-// sample data miss Lea Aspera
-$self_assessed_scores = $comp->get_self_assessed_record(1)["data"];
-$sup_assessed_scores = $comp->get_sup_assessed_record(1)["data"];
+// $exp = unserialize($row["experience"]);
 
-// print("<pre>".print_r($self,true)."</pre>");
-echo json_encode($self_assessed_scores);
-echo "<br/>";
-
-$data = $comp->gen_report($self_assessed_scores,$sup_assessed_scores);
-
-print("<pre>".print_r($data,true)."</pre>");
+print("<pre>".print_r($row,true)."</pre>");
 
 
+$exp = unserialize($row["experience"]);
 
+print("<pre>".print_r($exp,true)."</pre>");
