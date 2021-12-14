859a0c11ed157c96c6328e5d4edd25acda04ca12
fjvalencia <franzval123@gmail.com>
2021-12-14 11:56:03 +0800

    list of capability development program now ordered descending by date.

--- lgusponsoredtraining_proc.php
+++ lgusponsoredtraining_proc.php
@@ -14,14 +14,27 @@ if (isset($_POST["load"])) {
 	$paxTotal = 0;
 	$budgetTotal = 0;
 	if ($result->num_rows == 0) {
-		?>
+?>
 		<tr>
 			<td colspan="10" style="text-align: center; color: lightgrey;">No Data</td>
 		</tr>
-		<?php
+	<?php
 	}
 	$count = 1;
+
+	$data = [];
 	while ($row = $result->fetch_assoc()) {
+		$data[] = $row;
+	}
+
+	$data = add_sortable_date($data);
+
+	usort($data, function ($item1, $item2) {
+		return $item2['sortable_date'] <=> $item1['sortable_date'];
+	});
+
+	foreach ($data as $row) {
+		// while ($row = $result->fetch_assoc()) {
 		$row_id = $row["row_id"];
 		$activityProcess = $row["activityProcess"];
 		$accountablePerson = $row["accountablePerson"];
@@ -34,76 +47,68 @@ if (isset($_POST["load"])) {
 		$budgetTotal += $budget;
 		$expectedOutputs = $row["expectedOutputs"];
 		$remarks = $row["remarks"];
-		?>
+	?>
 		<tr>
-			<td><?php echo $count++;?></td>
-			<td><?php echo $activityProcess;?></td>
-			<td><?php echo $accountablePerson;?></td>
-			<td><?php echo $daysNum;?></td>
-			<td><?php echo $paxNum;?></td>
-			<td><?php echo $venue;?></td>
-			<td><?php echo $date;?></td>
-			<td><?php echo "&#8369;".number_format($budget,2,".",",");?></td>
-			<td><?php echo $expectedOutputs;?></td>
-			<td><?php echo $remarks;?></td>
+			<td><?php echo $count++; ?></td>
+			<td><?php echo $activityProcess; ?></td>
+			<td><?php echo $accountablePerson; ?></td>
+			<td><?php echo $daysNum; ?></td>
+			<td><?php echo $paxNum; ?></td>
+			<td><?php echo $venue; ?></td>
+			<td><?php echo $date; ?></td>
+			<td><?php echo "&#8369;" . number_format($budget, 2, ".", ","); ?></td>
+			<td><?php echo $expectedOutputs; ?></td>
+			<td><?php echo $remarks; ?></td>
 			<td class="noprint">
 				<div class="ui icon basic mini buttons">
-					<button onclick="editFunc('<?php echo $row_id;?>')" title="Quick Edit" class="ui button"><i class="edit outline icon"></i></button>
-					<button onclick="deleteFunc('<?php echo $row_id;?>')" title="Delete" class="ui button"><i class="trash alternate outline icon"></i></button>
+					<button onclick="editFunc('<?php echo $row_id; ?>')" title="Quick Edit" class="ui button"><i class="edit outline icon"></i></button>
+					<button onclick="deleteFunc('<?php echo $row_id; ?>')" title="Delete" class="ui button"><i class="trash alternate outline icon"></i></button>
 				</div>
 			</td>
 		</tr>
-		<?php
+	<?php
 	}
 	?>
-<!-- 	<tr>
-		<td colspan="10"><button class="ui mini fluid green button noprint" onclick="addNew('<?php echo $act_id;?>')">Add</button></td>
+	<!-- 	<tr>
+		<td colspan="10"><button class="ui mini fluid green button noprint" onclick="addNew('<?php echo $act_id; ?>')">Add</button></td>
 	</tr> -->
 	<tr style="text-align: right;">
 		<th colspan="4">TOTAL NO. OF EMPLOYEES WITH L&D INTERVENTION:</th>
-		<th><?php echo $paxTotal;?></th>
+		<th><?php echo $paxTotal; ?></th>
 		<th colspan="2">TOTAL AMOUNT:</th>
-		<th><?php echo "&#8369;".number_format($budgetTotal,2,".",",");?></th>
+		<th><?php echo "&#8369;" . number_format($budgetTotal, 2, ".", ","); ?></th>
 		<th>RUNNING BALANCE:</th>
-		<th><?php 
-		$runningBal = $allocatedBudget-$budgetTotal;
-		echo "&#8369;".number_format($runningBal,2,".",",");
-		?>
+		<th><?php
+			$runningBal = $allocatedBudget - $budgetTotal;
+			echo "&#8369;" . number_format($runningBal, 2, ".", ",");
+			?>
 		</th>
 	</tr>
 <?php
-} 
-
-elseif (isset($_POST["loadBudget"])) {
+} elseif (isset($_POST["loadBudget"])) {
 	$act_id = $_POST["act_id"];
 	$sql = "SELECT `allocatedBudget` FROM `ldactivities` WHERE `act_id` = '$act_id'";
 	$result = $mysqli->query($sql);
 	$row = $result->fetch_assoc();
 	$allocatedBudget = $row["allocatedBudget"];
-	echo number_format($allocatedBudget,2,".",",");
-}
-
-elseif (isset($_POST["loadYear"])) {
+	echo number_format($allocatedBudget, 2, ".", ",");
+} elseif (isset($_POST["loadYear"])) {
 	$act_id = $_POST["act_id"];
 	$sql = "SELECT `year` FROM `ldactivities` WHERE `act_id` = '$act_id'";
 	$result = $mysqli->query($sql);
 	$row = $result->fetch_assoc();
 	$year = $row["year"];
 	echo $year;
-}
-
-elseif (isset($_POST["editAllocatedBudget"])) {
+} elseif (isset($_POST["editAllocatedBudget"])) {
 	$act_id = $_POST["act_id"];
 	$allocatedBudget = $_POST["allocatedBudget"];
 	if (!is_numeric($allocatedBudget)) {
-		$moneyChar = array("₱",",","Php","P","p","Php.","P.","p.");
+		$moneyChar = array("₱", ",", "Php", "P", "p", "Php.", "P.", "p.");
 		$allocatedBudget = str_replace($moneyChar, "", $allocatedBudget);
 	}
 	$sql = "UPDATE `ldactivities` SET `allocatedBudget` = '$allocatedBudget' WHERE `ldactivities`.`act_id` = '$act_id'";
 	$mysqli->query($sql);
-}
-
-elseif (isset($_POST["addNew"])) {
+} elseif (isset($_POST["addNew"])) {
 	$act_id = $_POST["act_id"];
 	$activityProcess = addslashes($_POST["activityProcess"]);
 	$accountablePerson = addslashes($_POST["accountablePerson"]);
@@ -117,15 +122,11 @@ elseif (isset($_POST["addNew"])) {
 
 	$sql = "INSERT INTO `ldactivitieslist` (`row_id`, `act_id`, `activityProcess`, `accountablePerson`, `daysNum`, `paxNum`, `venue`, `date`, `budget`, `expectedOutputs`, `remarks`) VALUES (NULL, '$act_id', '$activityProcess', '$accountablePerson', '$daysNum', '$paxNum', '$venue', '$date', '$budget', '$expectedOutputs', '$remarks')";
 	$mysqli->query($sql);
-}
-
-elseif (isset($_POST["deleteFunc"])) {
+} elseif (isset($_POST["deleteFunc"])) {
 	$row_id = $_POST["row_id"];
 	$sql = "DELETE FROM `ldactivitieslist` WHERE `ldactivitieslist`.`row_id` = '$row_id'";
 	$mysqli->query($sql);
-}
-
-elseif (isset($_POST["loadInit"])) {
+} elseif (isset($_POST["loadInit"])) {
 	$row_id = $_POST["row_id"];
 	$sql = "SELECT * FROM `ldactivitieslist` WHERE `row_id` = '$row_id'";
 	$result = $mysqli->query($sql);
@@ -140,12 +141,10 @@ elseif (isset($_POST["loadInit"])) {
 	$expectedOutputs = $row["expectedOutputs"];
 	$remarks = $row["remarks"];
 
-	$json = array('activityProcess' => $activityProcess,'accountablePerson' => $accountablePerson,'daysNum' => $daysNum,'paxNum' => $paxNum,'venue' => $venue,'date' => $date,'budget' => $budget,'expectedOutputs' => $expectedOutputs,'remarks' => $remarks);
+	$json = array('activityProcess' => $activityProcess, 'accountablePerson' => $accountablePerson, 'daysNum' => $daysNum, 'paxNum' => $paxNum, 'venue' => $venue, 'date' => $date, 'budget' => $budget, 'expectedOutputs' => $expectedOutputs, 'remarks' => $remarks);
 
 	echo json_encode($json);
-}
-
-elseif (isset($_POST["editFunc"])) {
+} elseif (isset($_POST["editFunc"])) {
 	$row_id = $_POST["row_id"];
 	$activityProcess = addslashes($_POST["activityProcess"]);
 	$accountablePerson = addslashes($_POST["accountablePerson"]);
@@ -159,5 +158,38 @@ elseif (isset($_POST["editFunc"])) {
 	$sql = "UPDATE `ldactivitieslist` SET `activityProcess`='$activityProcess',`accountablePerson`='$accountablePerson',`daysNum`='$daysNum',`paxNum`='$paxNum',`venue`='$venue',`date`='$date',`budget`='$budget',`expectedOutputs`='$expectedOutputs',`remarks`='$remarks' WHERE `row_id`= '$row_id'";
 	$mysqli->query($sql);
 }
+
+
+
+function add_sortable_date($data)
+{
+	if (!$data) return [];
+	$data = $data;
+	foreach ($data as $key => $value) {
+		$date = $value["date"];
+		// $data[$key]["date_arr"] = preg_split('/[\s\,\-]+/', $date);
+		$data[$key]["sortable_date"] = create_time($date);
+	}
+	// parse string date to sortable date end
+	return $data;
+}
+
+function create_time($date)
+{
+	$err = 9999999999;
+	if (!$date) return $err;
+	$arr = preg_split('/[\s\,\-]+/', $date);
+	$month = $arr[0] ? substr($arr[0], 0, 3) : '01';
+	$day = $arr[1] ? substr($arr[1], 0, 2) : '01';
+	// get length of arr
+	$length = count($arr);
+	$year = $length > 0 ? $arr[$length - 1] : 9999;
+	$date_str = $month . $day . "," . $year;
+	return strtotime($date_str);
+}
+
+
+
+
 
 ?>
--- test.php
+++ test.php
@@ -1,24 +1,64 @@
 <?php
-require_once "libs/Competency.php";
-require_once "libs/Department.php";
-require_once "libs/LeaveRecordsController.php";
-require_once "libs/CalendarController.php";
+require_once "_connect.db.php";
+$date = "Feb20,2021";
+echo strtotime($date);
+echo "<br>";
+$date = "Feb21,2021";
+echo strtotime($date);
+echo "<br>";
 
-$calendar = new CalendarController;
-$comp = new Competency;
-$dept = new Department;
-$leave = new LeaveRecordsController;
-$leave->set_employee(9);
+$sql = "SELECT * FROM `ldactivitieslist` WHERE `act_id` = '10'";
+$result = $mysqli->query($sql);
 
-$array_data = [];
-// $calendar->set_employee_id(21805);
-// $array_data = $calendar->get_all_events();
+$data = array();
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = array(
+        "date" => $row ["date"]
+    );
+}
+
+// parse string date to sortable date start
 
 
-$array_data = $leave->get_summary();
-// $array_data = $comp->generate_report_by_dept_id(7);
-// $array_data = $dept->get_departments();
-// $array_data = $comp->get_in_depth_data(14);
-// var_dump($array_data);
-print("<pre>".print_r($array_data,true)."</pre>");
+
+$data = add_sortable_date($data);
+
+usort($data, function ($item1, $item2) {
+    return $item2['sortable_date'] <=> $item1['sortable_date'];
+});
+
+print("<pre>".print_r($data,true)."</pre>");
+
+
+// 1613836800
+// 9999999999
+
+
+
+function add_sortable_date($data) {
+    if (!$data) return [];
+    $data = $data;
+    foreach ($data as $key=>$value) {
+        $date = $value["date"];
+        // $data[$key]["date_arr"] = preg_split('/[\s\,\-]+/', $date);
+         $data[$key]["sortable_date"] = create_time($date);
+    }
+    // parse string date to sortable date end
+    return $data;
+}
+
+function create_time($date){
+    $err = 9999999999;
+    if (!$date) return $err;
+    $arr = preg_split('/[\s\,\-]+/', $date);
+    $month = $arr[0]?substr($arr[0],0,3):'01';
+    $day = $arr[1]?substr($arr[1],0,2):'01';
+    // get length of arr
+    $length = count($arr);
+    $year = $length>0?$arr[$length-1]:9999;
+    $date_str = $month.$day.",".$year;
+    return strtotime($date_str);
+}
+
 
